a88dc6619194ac05a2c6d2a84f9bbfef4ff055e5
shawnho1018 <shawnho@google.com>
2022-10-15 06:22:22 +0000

    local change

--- examples/3_demo-binaryautz/1_demo.sh
+++ examples/3_demo-binaryautz/1_demo.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-
 DEMO_FOLDER=$(pwd)
+# Use skaffold build with cloudbuild profile to get have the image signed for SLSA-L3
 pushd ../../
 skaffold build -p cloudbuild --file-output $DEMO_FOLDER/image.json
 popd
-IMAGE=$(cat image.json | jq -r '.builds[0].tag')
+IMAGE=$(cat image.json | jq -r '.builds[0].tag' | sed 's/\:latest//g')
+
+# Retrieve the provenance.json
 gcloud artifacts docker images describe ${IMAGE} --format json --show-provenance > provenance.json
 SOURCE_URI=$(cat provenance.json | jq -r '.provenance_summary.provenance[0].build.intotoStatement.slsaProvenance.materials[0].uri')
-slsa-verifier verify-image "$IMAGE" \
+
+# Verify Image
+./slsa-verifier verify-image "$IMAGE" \
 --provenance-path provenance.json \
 --source-uri ${SOURCE_URI} \
 --builder-id=https://cloudbuild.googleapis.com/GoogleHostedWorker
+
